a155a8fe5c98a49872b3e155b46c3da260702d14
Aditya Vashishtha <avtmax007@gmail.com>
2023-07-28 19:21:45 +0530

    Acceleration and deceleration

--- Rage Driving/Assets/Scripts/CarController.cs
+++ Rage Driving/Assets/Scripts/CarController.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
  
-    public float speed = 10f;           
+    public float maxSpeed = 100f;           
     public float rotationSpeed = 100f;
+    public float acceleration = 10f;                       
+    public float deceleration = 10f;
 
     public AudioClip honkSound;
 
     private float horizontalInput;     
     private float verticalInput;
+    private float currentSpeed;
 
     public float deathZone = -10f; 
     public Vector3 startingPosition;
@@ -52,7 +55,22 @@ public class CarController : MonoBehaviour
     private void MoveCar()
     {
         // Car's forward movement
-        Vector3 movement = transform.forward * verticalInput * speed * Time.fixedDeltaTime;
+        Vector3 movement = transform.forward * verticalInput * acceleration * Time.fixedDeltaTime;
+
+        // Deceleration when not accelerating
+        if (Mathf.Approximately(verticalInput, 0f))
+        {
+            float decelerationAmount = deceleration * Time.fixedDeltaTime;
+            float speedSign = Mathf.Sign(currentSpeed);
+            currentSpeed = Mathf.Clamp(Mathf.Abs(currentSpeed) - decelerationAmount, 0f, maxSpeed) * speedSign;
+            movement = transform.forward * currentSpeed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            // Accelerate the car and cap the speed at the maxSpeed
+            currentSpeed = Mathf.Clamp(currentSpeed + (verticalInput * acceleration * Time.fixedDeltaTime), 0f, maxSpeed);
+            movement = transform.forward * currentSpeed * Time.fixedDeltaTime;
+        }
 
         // Movement to car's Rigidbody
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + movement);
@@ -84,7 +102,9 @@ public class CarController : MonoBehaviour
         // Reset the car's position and rotation to the starting position
         transform.position = startingPosition;
         transform.rotation = Quaternion.identity;
-        
+
+        // Reset the current speed to zero
+        currentSpeed = 0f;
     }
 }
 
